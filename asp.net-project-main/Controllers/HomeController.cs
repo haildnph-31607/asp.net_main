115d9a750c555906d46ee345f8f93dbc51a88b31
haildnph-31607 <nguyenminhkhong2004@gmail.com>
2024-08-25 19:23:38 +0700

    client product

--- asp.net-project-main/Controllers/HomeController.cs
+++ asp.net-project-main/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using asp.net_project_main.Models;
+using asp.net_project_main.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace asp.net_project_main.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    protected readonly aspnet_project_mainContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, aspnet_project_mainContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var asp_data_product = _context.Product.Include(p => p.Category);
+
+        return View(await asp_data_product.ToListAsync());
     }
 
     public IActionResult Privacy()
@@ -28,4 +34,24 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public async Task<IActionResult> Details(int? id)
+    {
+		if (id == null)
+		{
+			return NotFound();
+		}
+
+		var aspnetProduct = await _context.Product.FirstOrDefaultAsync(p => p.Id == id);
+		if (aspnetProduct == null)
+		{
+			return NotFound();
+		}
+
+		return View(aspnetProduct);
+	} 
+    public IActionResult Product()
+    {
+        return View();
+    }
 }
+
